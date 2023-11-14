90da7e3b23db147be1f0f3c571a28a2ee31de09d
Tenzin Tshomo <tenzintsomoo.66@gmail.com>
2023-11-14 18:56:20 +0600

    story nav
    Signed-off-by: Tenzin Tshomo <tenzintsomoo.66@gmail.com>

--- resources/views/components/story_nav.blade.php
+++ resources/views/components/story_nav.blade.php
@@ -1,11 +1,11 @@
 <div id="fullpage-story-menu" class="story-nav zoom-in-element">
     <ul class="list-none story-nav-list">
-        <li><a class="{{ $dark ? 'text-white' : 'text-black' }}" href="/the-kingdom">The Kindom</a></li>
-        <li><a class="{{ $dark ? 'text-white' : 'text-black' }}" href="/the-flora">The Flora</a></li>
-        <li><a class="{{ $dark ? 'text-white' : 'text-black' }}" href="/the-fauna">The Faun</a></li>
-        <li><a class="{{ $dark ? 'text-white' : 'text-black' }}" href="/the-climate">The Climate</a></li>
-        <li><a class="{{ $dark ? 'text-white' : 'text-black' }}" href="/the-change">The Change</a></li>
-        <li><a class="{{ $dark ? 'text-white' : 'text-black' }}" href="/the-laws">The Laws</a></li>
-        <li><a class="{{ $dark ? 'text-white' : 'text-black' }}" href="/the-people">The People</a></li>
+        <li><a href="/the-kingdom">The Kindom</a></li>
+        <li><a href="/the-flora">The Flora</a></li>
+        <li><a href="/the-fauna">The Faun</a></li>
+        <li><a href="/the-climate">The Climate</a></li>
+        <li><a href="/the-change">The Change</a></li>
+        <li><a href="/the-laws">The Laws</a></li>
+        <li><a href="/the-people">The People</a></li>
     </ul>
 </div>
