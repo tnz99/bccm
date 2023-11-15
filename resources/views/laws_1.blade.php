3f1e49e648d8305ea4bb85719fd1d7273beb9fb0
Tenzin Tshomo <tenzintsomoo.66@gmail.com>
2023-11-15 21:54:35 +0600

    merge tenzin changes
    Signed-off-by: Tenzin Tshomo <tenzintsomoo.66@gmail.com>

--- resources/views/laws_1.blade.php
+++ resources/views/laws_1.blade.php
@@ -7,18 +7,18 @@
 <div class="d-flex">
     <div>
         <div id="main-section" class="d-flex justify-content-between vw-65 year-story-wrapper">
-            <div class="ml-146">
+            <div class="year-story-header">
                 <div class="p-0">
-                    <h2 class="text-black fw-700">1995</h2>
-                    <p class="text-black fs-20 fw-700 font-normal lh-normal mt-22" style="width: 25ch;">Bhutan's Forest and Nature Conservation Act - "Conservation and sustainable management of Bhutan's forests."</p>
+                    <h2 class="text-black fw-700 year-story-header-year">1995</h2>
+                    <p class="text-black fs-20 fw-700 font-normal lh-normal mt-22 year-story-header-subtext" style="width: 25ch;">Bhutan's Forest and Nature Conservation Act - "Conservation and sustainable management of Bhutan's forests."</p>
                 </div>
             </div>
             
-            <div class="mt-14 ml-9">
-                <ul class="m-0 text-black fw-400 lh-normal font-normal fs-14" style="width: 35ch;">
-                    <li class="p-1">The act has helped in the conservation and sustainable management of Bhutan's forests, which cover over 70% of the country's land area.</li>
-                    <li class="p-1">Bhutan has maintained a high forest coverage, which contributes to carbon sequestration and helps mitigate climate change.</li>
-                    <li class="p-1">The act has facilitated the protection of biodiversity, including endangered species like the Bengal tiger and snow leopard.</li>
+            <div class="ml-9">
+                <ul class="fs-14 year-story-content" style="width: 35ch;">
+                    The act has helped in the conservation and sustainable management of Bhutan's forests, which cover over 70% of the country's land area
+                    Bhutan has maintained a high forest coverage, which contributes to carbon sequestration and helps mitigate climate change.
+                    The act has facilitated the protection of biodiversity, including endangered species like the Bengal tiger and snow leopard.
                 </ul>
             </div>
         </div>
@@ -42,10 +42,10 @@
         </div> 
     </div>   
 
-    <img id="prayer-wheel" src="{{ asset('images/prayer-wheel/1.png') }}" alt="prayer wheel" class="position"  >
+    <img id="prayer-wheel" src="{{ asset('images/prayer-wheel/1.png') }}" alt="prayer wheel" class="position prayer-wheel"  >
 </div>
 @endsection
 
 @section('nav')
-    <x-circular_nav background="the-law-cnav-bg" inner-border="broder-gray"/>
+    <x-circular_nav background="the-law1-cnav-bg" inner-border="broder-gray"/>
 @endsection
