bf6d321e83aa828f6db45414444f098c088e34e2
Tenzin Tshomo <tenzintsomoo.66@gmail.com>
2023-11-19 23:29:34 +0600

    autumn animation
    Signed-off-by: Tenzin Tshomo <tenzintsomoo.66@gmail.com>

--- resources/views/autumn.blade.php
+++ resources/views/autumn.blade.php
@@ -17,6 +17,111 @@
 @endsection
 
 @section('nav')
+<script>
+        // var colors = ["#227C70", "#FFBF00"];
+    var colors = ["#227C70", "#FFBF00","#0002A1"];
+
+    var id = null;
+    function frame() {
+        confetti({
+            particleCount: 2,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 },
+            colors: colors,
+        });
+        confetti({
+            particleCount: 2,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 },
+            colors: colors,
+        });
+
+        if (Date.now() < Date.now() + 15000) {
+            id =  requestAnimationFrame(frame);
+        }
+    }
+
+    window.onload = snow();
+    
+
+    $(document).ready(function() {
+           
+        $("#b-close").click(function() {
+            // alert("page is loaded");
+            $(".popup_this_container").hide();
+            window.cancelAnimationFrame(id);
+        });
+    });
+
+    function stop(){
+        window.cancelAnimationFrame(id);
+
+        setTimeout(() => {
+        confetti.reset();
+    }, 100);
+}
+
+function fireworks() {
+        var duration = 25 * 1000;
+        var animationEnd = Date.now() + duration;
+        var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+        function randomInRange(min, max) {
+          return Math.random() * (max - min) + min;
+        }
+
+        var interval = setInterval(function () {
+          var timeLeft = animationEnd - Date.now();
+
+          if (timeLeft <= 0) {
+            return clearInterval(interval);
+          }
+
+          var particleCount = 50 * (timeLeft / duration);
+          // since particles fall down, start a bit higher than random
+          confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+          confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+        }, 250);
+      }
+
+function snow() {
+        var duration = 15 * 1000;
+        var animationEnd = Date.now() + duration;
+        var skew = 1;
+
+        function randomInRange(min, max) {
+          return Math.random() * (max - min) + min;
+        }
+
+        (function frame() {
+          var timeLeft = animationEnd - Date.now();
+          var ticks = Math.max(200, 500 * (timeLeft / duration));
+          skew = Math.max(0.8, skew - 0.001);
+
+          confetti({
+            particleCount: 1,
+            startVelocity: 0,
+            ticks: ticks,
+            origin: {
+              x: Math.random(),
+              // since particles fall down, skew start toward the top
+              y: (Math.random() * skew) - 0.2
+            },
+            colors: ['#FF5722'],
+            shapes: ['circle'],
+            gravity: randomInRange(0.4, 0.6),
+            scalar: randomInRange(0.4, 1),
+            drift: randomInRange(-0.4, 0.4)
+          });
+
+          if (timeLeft > 0) {
+            requestAnimationFrame(frame);
+          }
+        }());
+      }
+</script>
     <x-story-nav :dark=$darkThemeFlag />
     <x-circular_nav :background="$cnavBg" :inner-border="$cnavInnerBorder"/>
 @endsection
